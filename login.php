1f2511cc60c69a65de3b776461a2762b0685a15c
Leorna <gabrielcapuerj@gmail.com>
2019-09-01 01:02:09 -0300

    little changes

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <html>
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="foundation-6.5.1-complete/css/foundation.css">
